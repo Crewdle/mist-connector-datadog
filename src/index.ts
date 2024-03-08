aabb64b9750fd0c781c7e4d612824412d93dab14
Vincent Lamanna <vincent@crewdle.com>
2024-03-08 11:01:40 -0500

    Add Typedoc documentation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,18 @@ import { datadogLogs } from '@datadog/browser-logs';
 
 import { ILoggingConnector, LoggingConnectorEvent } from '@crewdle/web-sdk';
 
+/**
+ * The Datadog logging connector - Easily log your Crewdle Mist cluster events to Datadog
+ * @category Connector
+ */
 export class DatadogLoggingConnector implements ILoggingConnector {
+  /**
+   * Create a new Datadog logging connector.
+   * @param clientToken The Datadog client token
+   * @param site The Datadog site
+   * @param env The environment
+   * @param version The version
+   */
   constructor(clientToken: string, site: string, env?: string, version?: string) {
     datadogLogs.init({
       clientToken,
@@ -13,14 +24,29 @@ export class DatadogLoggingConnector implements ILoggingConnector {
     });
   }
 
+  /**
+   * Send a log message to Datadog.
+   * @param message The log message
+   * @param event The event
+   */
   public log(message: string, event: LoggingConnectorEvent): void {
     datadogLogs.logger.info(message, event);
   }
 
+  /**
+   * Send a warning message to Datadog.
+   * @param message The warning message
+   * @param event The event
+   */
   public warn(message: string, event: LoggingConnectorEvent): void {
     datadogLogs.logger.warn(message, event);
   }
 
+  /**
+   * Send an error message to Datadog.
+   * @param message The error message
+   * @param event The event
+   */
   public error(message: string, event: LoggingConnectorEvent): void {
     datadogLogs.logger.error(message, event);
   }
