5d5d260d3b4130bc0aac1a8c2d02d69a914fa0e6
Vincent Lamanna <vincent@crewdle.com>
2024-03-13 11:56:40 -0400

    Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { datadogLogs } from '@datadog/browser-logs';
 
-import { ILoggingConnector, LoggingConnectorEvent } from '@crewdle/web-sdk';
+import type { ILoggingConnector, LoggingConnectorEvent } from '@crewdle/web-sdk';
 
 /**
  * The Datadog logging connector - Easily log your Crewdle Mist cluster events to Datadog
